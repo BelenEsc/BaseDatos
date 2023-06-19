27014855514faf81df511b951177f857f8cc459d
BelenEsc <belenescobari@gmail.com>
2023-06-19 23:49:26 +0200

    casa nada

--- src/aplicacion/AccesoBD.java
+++ src/aplicacion/AccesoBD.java
@@ -1,8 +1,11 @@
 package aplicacion;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
@@ -29,34 +32,53 @@ class Marco extends JFrame {
 
 		setTitle("Consulta BBDD");
 		setBounds(50, 50, 500, 500);
-
-		setLayout(new BorderLayout());
-
 		Lamina lamina = new Lamina();
-		add(lamina, BorderLayout.NORTH);
-		JTextArea areaMostrar = new JTextArea();
-		add(areaMostrar, BorderLayout.CENTER);
-		JButton buscar = new JButton("Buscar");
-		add(buscar, BorderLayout.SOUTH);
+		add(lamina);
+
 
 	}
 }
 
 class Lamina extends JPanel {
+	
+	JComboBox<String> caja1;
+	JComboBox<String> caja2;
+	PreparedStatement stat;
+	String consulta = ("SELECT * FROM TABLES WHERE ROW_FORMAT=?");
+	Connection miConexion;
+	ResultSet rs = null;
+	JTextArea areaMostrar; 
 
 	public Lamina() {
-		JComboBox<String> caja1 = new JComboBox<String>();
+		setLayout(new BorderLayout());
+		
+		JPanel menus = new JPanel();
+		
+		caja1 = new JComboBox<String>();
 		caja1.addItem("todos");
 
-		JComboBox<String> caja2 = new JComboBox<String>();
+		caja2 = new JComboBox<String>();
 		caja2.addItem("Todos");
 
-		add(caja1);
-		add(caja2);
+		areaMostrar= new JTextArea();
+		JButton buscar = new JButton("Buscar");
+		buscar.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				ejecutar();
+			}
+		});
+		menus.add(caja1);
+		menus.add(caja2);
+		add(menus,BorderLayout.NORTH);
+		add(areaMostrar,BorderLayout.CENTER);
+		add(buscar, BorderLayout.SOUTH);
 
 		// conectar a la base de datos
 		try {
-			Connection miConexion = DriverManager.getConnection(
+			miConexion= DriverManager.getConnection(
 					"jdbc:mysql://sql7.freemysqlhosting.net:3306/information_schema", "sql7623827", "akYVms4umE");
 
 			Statement miSentencia = miConexion.createStatement();
@@ -79,6 +101,32 @@ class Lamina extends JPanel {
 			}
 			resultado.close();
 
+		} catch (Exception e) {
+		}
+	}
+
+
+	private void ejecutar() {
+		try {
+			String seccion = caja1.getSelectedItem().toString();
+			stat=miConexion.prepareStatement(consulta);
+			stat.setString(1, seccion);
+			rs=stat.executeQuery();
+			while (rs.next()) {
+				areaMostrar.append(rs.getString(1));
+				areaMostrar.append(", ");
+				areaMostrar.append(rs.getString(2));
+				areaMostrar.append(", ");
+				areaMostrar.append(rs.getString(3));
+				areaMostrar.append(", ");
+				areaMostrar.append(rs.getString(4));
+				areaMostrar.append(", ");
+				areaMostrar.append(rs.getString(5));
+				areaMostrar.append(", ");
+				areaMostrar.append("\n");
+				
+			}
+			
 		} catch (Exception e) {
 		}
 
